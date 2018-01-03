e421244a4522fbe6bf6d9ede4e53a18f4061bc91
Denis Khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2018-01-03 17:17:30 +0300

    added custom pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,30 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoginPageModule } from '../pages/login/login.module';
+import { MenuPageModule } from '../pages/menu/menu.module';
+import { SpecialPageModule } from '../pages/special/special.module';
+import { Tab1PageModule } from '../pages/tab1/tab1.module';
+import { Tab2PageModule } from '../pages/tab2/tab2.module';
+import { TabsPageModule } from '../pages/tabs/tabs.module';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
+    LoginPageModule,
+    MenuPageModule,
+    SpecialPageModule,
+    Tab1PageModule,
+    Tab2PageModule,
+    TabsPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
